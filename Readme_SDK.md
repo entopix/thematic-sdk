de298044ed0900690e9f1451e82d5db8abfc5aae
Nathan Holmberg <nathan@entopix.com>
2016-01-20 16:37:50 +1300

    Initial Commit

---
+++ Readme_SDK.md
@@ -0,0 +1,157 @@
+# Python SDK
+
+The Python SDK to use the Thematic API is distributed with the following artifacts:
+
+- rest_interface.py, an SDK for the API written in Python
+- config.ini, a sample config file containing settings and parameters for the API
+
+These files are helpful in understanding how to call the API and how to perform its different functions.
+The following sections explain how to log in, how to process different types of surveys, how to tweak the Model, and finally how to perform incremental updates.
+
+##  Initialising the SDK
+
+Assuming that API base URL, username and password are stored in config.ini, use the following code to initialise the REST interface of the API:
+
+```
+cfg = ConfigParser.ConfigParser()
+cfg.read('config.ini')
+rest_if = rest_interface( cfg.get('server', 'base_url'), cfg.get('server', 'username'), cfg.get('server', 'password'))
+```
+
+## Analysing Surveys
+
+To analyse a Survey Instance, a new survey needs creating:
+
+```
+survey_info = rest_if.create_survey(cfg.get('survey', 'name'),
+                                cfg.get('survey', 'total_columns'),
+                                json.loads(cfg.get('survey', 'columns')),
+                                cfg.get('survey', 'has_header'),
+                                cfg.get('survey', 'modelset_id'))
+```
+
+The last element, model_id, is optional. Use it to reference a Model Set from another survey instance, such as the first instance of a repeating survey.
+
+The format that survey columns should be a list of lists of dictionaries. Details of each:
+* outer list: this outer definition is just to contain all items
+* each internal list: a group of columns that share common themes. Most surveys will only have one internal list
+* each dictionary proper json format. The dictionaries should be of the form:
+{ "index" : integer_column_index, "name" : "human readable name" }
+
+
+The return object will contain survey id and modelset id. 
+
+```
+survey_id = survey_info["survey_id"]
+modelset_id = survey_info["modelset_id"]
+```
+
+Once a survey id is known, the SDK allows to pass a path to a file containing survey results. While **run_job** method passes 
+this file to the job creating stage, the next method, **wait_for_job_completion**, halts the program until the job is completed:
+
+```
+filename = cfg.get('modelset', 'initialfile')
+job_id = rest_if.run_job( survey_id, filename )
+rest_if.wait_for_job_completion( job_id )
+```
+
+The following snippet shows how to retrieve the results of a job, which completes the analysis and lets you save the content on disk for inspection: 
+
+```
+results = rest_if.retrieve_results( job_id )
+if not results:
+    raise Exception ("Failed to retrieve results")
+base_filename, extension = os.path.splitext(os.path.basename(filename))
+csv_filename = os.path.join("data_out/",base_filename+"_output.csv")
+themes_filename = os.path.join("data_out/",base_filename+"_themes.json")
+with open( csv_filename, "w" ) as f:
+    f.write( results['csv'] )
+resulting_files.append( csv_filename )
+with open( themes_filename, "w" ) as f:
+    f.write( results['themes'] )
+resulting_files.append( themes_filename )
+```
+
+## Tweaking Analysis
+
+To tweak the analysis by editing the concepts file, first retrieve the automatically generated concepts file, and save it on disk for inspection:
+
+```
+concepts = rest_if.retrieve_concepts( job_id )
+concepts_out_filename = "data_out/concepts.json"
+with open( concepts_out_filename, "w" ) as f:
+    f.write( concepts )
+```
+
+After the concept file has been reviewed and, if necessary, updated, run a new job as follows:
+
+```
+concepts_out_filename = cfg.get('modelset', 'conceptsfile')
+survey_id = cfg.get('modelset','survey_id')
+previous_job = cfg.get('jobs','base_job')
+job_id = rest_if.configure_concepts( survey_id, concepts_out_filename, previous_job )
+rest_if.wait_for_job_completion( job_id )
+```
+
+Similarly, the SDK allows to retrieve the themes as follows: 
+
+```
+themes = rest_if.retrieve_themes( job_id ) )
+if not themes:
+    raise Exception ("Failed to retrieve themes")
+
+themes_filename = os.path.join("data_out/",base_filename+"_themes.json")
+with open( themes_filename, "w" ) as f:
+    f.write( themes )
+resulting_files.append( themes_filename )
+```
+
+After reviewing and modifying this file, a new Analysis using that file can be run as follows:
+
+```
+themes_out_filename = cfg.get('modelset', 'themesfile')
+survey_id = cfg.get('modelset','survey_id')
+previous_job = cfg.get('jobs','base_job')
+job_id = rest_if.configure_themes( survey_id, themes_out_filename, previous_job )
+rest_if.wait_for_job_completion( job_id )
+```
+
+Finally, as explained above, you can tweak the API Analysis parameters too. By using the SDK, first retrieve them as a dictionary.
+ 
+```
+# retrieve parameters
+params = rest_if.retrieve_parameters( job_id ) )
+if not params:
+    raise Exception ("Failed to retrieve params")
+```
+
+After reviewing and modifying the values in the dictionary, a new Analysis using these values can be run as follows:
+
+```
+survey_id = cfg.get('modelset','survey_id')
+previous_job = cfg.get('jobs','base_job')
+job_id = rest_if.configure_parameters( survey_id, params, previous_job )
+rest_if.wait_for_job_completion( job_id )
+```
+
+## Running incremental updates
+
+Once you have created a survey, run the initial Analysis job and simply want to analyse an additional small number of responses, 
+the SDK provides the **run_incremental_update** method:
+
+```
+filename = cfg.get('modelset', 'incremental_update_to_survey')
+job_id = rest_if.run_incremental_update( survey_id, filename )
+rest_if.wait_for_job_completion( job_id )
+
+```
+## Translating survey responses
+
+In order to translate survey responses, simply use the **run_translation** method:
+
+```
+filename = cfg.get('modelset', 'foreign_survey')
+job_id = rest_if.run_translations( survey_id, filename )
+rest_if.wait_for_job_completion( job_id )
+```
+
---
+++ thematic/thematic.py
@@ -0,0 +1,249 @@
+
+import json
+import time
+#pip
+import requests
+
+class Thematic:
+    def __init__(self, base_url, username, password):
+        self.base_url = base_url
+        self.retrieve_apikey( username, password )
+
+    def retrieve_apikey( self, username, password):
+        payload = {'username' : username, 
+                    'password' : password
+                  }
+        r = requests.post(self.base_url+"/login",
+                        data=payload)
+        response = json.loads(r.text)
+        if response["status"] != "success":
+            raise Exception("retrieve_apikey: Failed to login ("+response["error"]["message"]+")")
+        self.api_key = response["data"]["api_key"]
+
+
+
+    def create_survey( self, name, total_columns, columns, has_header,modelset_id=None,output_format=None):
+        payload = {'name' : name, 
+                    'total_columns' : total_columns, 
+                    'columns' : json.dumps(columns), 
+                    'has_header' : has_header}
+        # optional modelsetid
+        if modelset_id:
+            payload['modelset_id'] = modelset_id
+        if output_format:
+            payload['output_format'] = output_format
+        r = requests.post(self.base_url+"/create_survey",
+                        headers = {'X-API-Authentication' : self.api_key},
+                        data=payload)
+        response = json.loads(r.text)
+        if response["status"] != "success":
+            raise Exception("create_survey: Failed to create survey ("+response["error"]["message"]+")")
+        if "survey_id" not in response["data"]:
+            raise Exception("create_survey: Bad Response")
+        return response["data"]
+
+
+    def run_job( self, survey_id, csv_filename, previous_job_id=None ):
+        files = {'csv_file': open(csv_filename, 'rb')}
+        payload = { 'survey_id' : survey_id }
+        if previous_job_id:
+            payload["previous_job_id"] = previous_job_id
+        r = requests.post(self.base_url+"/create_job",
+                        headers = {'X-API-Authentication' : self.api_key},
+                        files=files,
+                        data=payload)
+        response = json.loads(r.text)
+        if response["status"] != "success":
+            raise Exception("run_job: Failed to create job ("+response["error"]["message"]+")")
+        if "jobid" not in response["data"]:
+            raise Exception("run_job: Bad Response")
+        return response["data"]["jobid"]
+
+    def run_incremental_update( self, survey_id, csv_filename ):
+        files = {'csv_file': open(csv_filename, 'rb')}
+        payload = { 'survey_id' : survey_id, 'job_type' : 'apply' }
+        r = requests.post(self.base_url+"/create_job",
+                        headers = {'X-API-Authentication' : self.api_key},
+                        files=files,
+                        data=payload)
+        response = json.loads(r.text)
+        if response["status"] != "success":
+            raise Exception("run_incremental_update: Failed to create job ("+response["error"]["message"]+")")
+        if "jobid" not in response["data"]:
+            raise Exception("run_incremental_update: Bad Response")
+        return response["data"]["jobid"]
+
+    def run_translations( self, survey_id, csv_filename ):
+        files = {'csv_file': open(csv_filename, 'rb')}
+        payload = { 'survey_id' : survey_id, 'job_type' : 'translate' }
+        r = requests.post(self.base_url+"/create_job",
+                        headers = {'X-API-Authentication' : self.api_key},
+                        files=files,
+                        data=payload)
+        response = json.loads(r.text)
+        if response["status"] != "success":
+            raise Exception("run_translations: Failed to create job ("+response["error"]["message"]+")")
+        if "jobid" not in response["data"]:
+            raise Exception("run_translations: Bad Response")
+        return response["data"]["jobid"]
+
+    def configure_concepts( self, concepts_filename, previous_job_id ):
+        files = {'concepts_file': open(concepts_filename, 'rb')}
+        payload = { }
+
+        r = requests.post(self.base_url+"/job/"+previous_job_id+"/concepts",
+                        headers = {'X-API-Authentication' : self.api_key},
+                        files=files,
+                        data=payload)
+        response = json.loads(r.text)
+        if response["status"] != "success":
+            raise Exception("configure_concepts: Failed to create configuration job ("+response["error"]["message"]+")")
+        if "jobid" not in response["data"]:
+            raise Exception("configure_concepts: Bad Response")
+        return response["data"]["jobid"]
+
+    def configure_themes( self, themes_filename, previous_job_id ):
+        files = {'themes_file': open(themes_filename, 'rb')}
+        payload = { }
+
+        r = requests.post(self.base_url+"/job/"+previous_job_id+"/themes",
+                        headers = {'X-API-Authentication' : self.api_key},
+                        files=files,
+                        data=payload)
+        response = json.loads(r.text)
+        if response["status"] != "success":
+            raise Exception("configure_themes: Failed to create configuration job ("+response["error"]["message"]+")")
+        if "jobid" not in response["data"]:
+            raise Exception("configure_themes: Bad Response")
+        return response["data"]["jobid"]
+
+    def configure_language_model( self, language_model_filename, previous_job_id ):
+        files = {'model_file': open(language_model_filename, 'rb')}
+        payload = { }
+
+        r = requests.post(self.base_url+"/job/"+previous_job_id+"/language_model",
+                        headers = {'X-API-Authentication' : self.api_key},
+                        files=files,
+                        data=payload)
+        response = json.loads(r.text)
+        if response["status"] != "success":
+            raise Exception("configure_language_model: Failed to create configuration job ("+response["error"]["message"]+")")
+        if "jobid" not in response["data"]:
+            raise Exception("configure_language_model: Bad Response")
+        return response["data"]["jobid"]
+
+    def configure_parameters( self, parameters, previous_job_id ):
+        payload = parameters
+
+        r = requests.post(self.base_url+"/job/"+previous_job_id+"/params",
+                        headers = {'X-API-Authentication' : self.api_key},
+                        data=payload)
+        response = json.loads(r.text)
+        if response["status"] != "success":
+            raise Exception("configure_parameters: Failed to create configuration job ("+response["error"]["message"]+")")
+        if "jobid" not in response["data"]:
+            raise Exception("configure_parameters: Bad Response")
+        return response["data"]["jobid"]
+
+    def get_job_details( self, job_id ):
+        r = requests.get(self.base_url+"/job/"+job_id+"/info",
+                    headers = {'X-API-Authentication' : self.api_key}
+                    )
+        response = json.loads(r.text)
+        if response["status"] != "success":
+            raise Exception("get_job_status: Failed to get job status ("+response["error"]["message"]+")")
+        if "state" not in response["data"]:
+            raise Exception("get_job_status: Bad Response")
+        return response["data"]
+
+    def get_charts( self, job_id, columns, format="json", compare_job_id=None, top_n=None, include_ignored=False):
+        payload = { 'columns' :json.dumps(columns), "format" : format, "compare_job_id" : compare_job_id, "top_n" : top_n, "include_ignored" : include_ignored }
+        r = requests.get(self.base_url+"/job/"+job_id+"/charts/",
+                    headers = {'X-API-Authentication' : self.api_key},
+                    params=payload
+                    )
+        response = json.loads(r.text)
+        if response["status"] != "success":
+            raise Exception("get_charts: Failed to get charts ("+response["error"]["message"]+")")
+        return response["data"]
+
+    def wait_for_job_completion( self, job_id ):
+        ready = False
+        print("Waiting for results...")
+        while not ready:
+            job_details = self.get_job_details( job_id )
+            status = job_details['state']
+            if status == "finished":
+                ready = True
+                break
+            elif status == "errored":
+                raise Exception("wait_for_job_completion: Job errored and did not complete")
+
+            time.sleep(2)
+
+    def list_jobs( self):
+        r = requests.get(self.base_url+"/jobs/",
+                    headers = {'X-API-Authentication' : self.api_key}
+                    )
+        if r.status_code != 200:
+            return None
+        response = json.loads(r.text)
+        return response["data"]
+
+
+    def retrieve_csv( self, job_id ):
+        r = requests.get(self.base_url+"/job/"+job_id+"/csv/",
+                    headers = {'X-API-Authentication' : self.api_key}
+                    )
+        if r.status_code != 200:
+            return None
+        return r.text.encode('utf-8')
+
+    def retrieve_themes( self, job_id ):
+        r = requests.get(self.base_url+"/job/"+job_id+"/themes/",
+                    headers = {'X-API-Authentication' : self.api_key}
+                    )
+        if r.status_code != 200:
+            return None
+        return r.text.encode('utf-8')
+
+
+    def retrieve_concepts( self, job_id ):
+        r = requests.get(self.base_url+"/job/"+job_id+"/concepts/",
+                    headers = {'X-API-Authentication' : self.api_key}
+                    )
+        if r.status_code != 200:
+            return None
+        return r.text.encode('utf-8')
+
+
+    def retrieve_excel( self, job_id, column, nps_column ):
+        r = requests.get(self.base_url+"/job/"+job_id+"/excel/"+str(column),
+                  headers = {'X-API-Authentication' : self.api_key},
+                       params={'nps_column' : nps_column}
+                  )
+        if r.status_code != 200:
+            return None
+        return r.content
+
+    def retrieve_language_model( self, job_id ):
+        r = requests.get(self.base_url+"/job/"+job_id+"/language_model/",
+                  headers = {'X-API-Authentication' : self.api_key}
+                  )
+        if r.status_code != 200:
+            return None
+        return r.content
+    
+
+    def retrieve_parameters( self, job_id ):
+        r = requests.get(self.base_url+"/job/"+job_id+"/params",
+                    headers = {'X-API-Authentication' : self.api_key}
+                    )
+        response = json.loads(r.text)
+        if response["status"] != "success":
+            raise Exception("get_job_status: Failed to get job status ("+response["error"]["message"]+")")
+        return response["data"]
+
+
+
+
