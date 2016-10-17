fe52aed0bd828c9693a3ef2e88022a420eb3a679
Nathan Holmberg <nathan@entopix.com>
2016-10-18 10:48:18 +1300

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name = 'thematic_sdk',
+    version = '1.0.0',
+    author='Thematic Ltd',
+    author_email='contact@getthematic.com',
+    url='http://getthematic.com/',
+    description = '',
+    packages=setuptools.find_packages(),
+    package_data = {},
+)
