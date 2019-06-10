bddf9f9c4069b64987f37339a6393a05aed8a1b2
Nathan Holmberg <nathan@getthematic.com>
2019-06-10 15:24:42 -0700

    INFRA-54: adding dependencies explicitly

--- setup.py
+++ setup.py
@@ -9,5 +9,9 @@ setuptools.setup(
     url='http://getthematic.com/',
     description = '',
     packages=setuptools.find_packages(),
-    package_data = {},
+    package_data = {},,
+    install_requires=[
+        "requests>=2.18",
+        "requests[security]"
+    ]
 )
